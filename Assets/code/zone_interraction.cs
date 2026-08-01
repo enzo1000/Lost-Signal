3cfea907956293c218cc67239533c779a92038c5
AlexCHPK <sachavidil@gmail.com>
2026-08-01 18:51:27 +0200

    ajout jeu allumettes

---
+++ Assets/code/zone_interraction.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class zone_interraction : MonoBehaviour
+{
+    private bool isPlayerInZone = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player") 
+        {
+            isPlayerInZone = true;
+            Debug.Log("Appuie sur E CHIENNNNNNN" + other.name);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            isPlayerInZone = false;
+            Debug.Log("TIE PARTIT SALE CHIEN !");
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+    {
+        if (isPlayerInZone && Keyboard.current.eKey.wasPressedThisFrame) 
+        {
+            IlAApuiyercecon();
+        }
+    }
+
+    void IlAApuiyercecon() 
+    {
+        Debug.Log("bravo ta appuie t trop fort bebou <3");
+
+    }
+}
