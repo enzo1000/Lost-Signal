0fa0a246b733a21fe4227e3c0694e1a4271b50b6
Enzo Martinez <killerbackofheart@gmail.com>
2026-08-01 14:32:15 +0200

    Add missing CIB changes

--- Assets/EnzoCode/PlayerMovement.cs
+++ Assets/EnzoCode/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
     [Header("Input Actions")]
     public InputActionReference m_moveAction;
     public InputActionReference m_lookAction;
-    private Vector2 m_turn;
 
+    private Vector2 m_turn;
     private Vector3 playerVelocity;
     private CharacterController m_cc = null;
 
@@ -36,14 +36,12 @@ public class PlayerMovement : MonoBehaviour
         Vector2 input = m_moveAction.action.ReadValue<Vector2>();
         Vector2 mousePos = m_lookAction.action.ReadValue<Vector2>();
 
-        Debug.Log(input.x + " " + input.y);
-
         m_turn.x += mousePos.x;
         m_turn.y += mousePos.y;
         transform.rotation = Quaternion.Euler(-m_turn.y, m_turn.x, 0);
 
         Vector3 move = transform.right * input.x + transform.forward * input.y;
-        move.y = 0.0f;
+        move.y = 0.0f;  // Move.y != input.y
         move = Vector3.ClampMagnitude(move, 1f);
 
         // Apply Gravity
