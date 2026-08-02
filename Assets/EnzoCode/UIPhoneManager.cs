494e4c08640b32c60c105a5390006cdb068528d0
Enzo Martinez <killerbackofheart@gmail.com>
2026-08-02 12:10:00 +0200

    Add first navigation in phone ui

---
+++ Assets/EnzoCode/UIPhoneManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class UIPhoneManager : MonoBehaviour
+{
+    public GameObject m_mainMenuFrame;
+    public GameObject m_mapMenuFrame;
+    public GameObject m_smsMenuFrame;
+
+    //================================================================================================================================
+    //================================================================================================================================
+    public void OpenTelephone() {
+        m_mainMenuFrame.SetActive(true);
+        //m_mapMenuFrame.SetActive(false);
+        m_smsMenuFrame.SetActive(false);
+    }
+
+    //================================================================================================================================
+    //================================================================================================================================
+    public void OnClickShowMap() {
+        m_mainMenuFrame.SetActive(false);
+        m_mapMenuFrame.SetActive(true);
+    }
+
+    //================================================================================================================================
+    //================================================================================================================================
+    public void OnClickShowSms() {
+        m_mainMenuFrame.SetActive(false);
+        m_smsMenuFrame.SetActive(true);
+    }
+}
